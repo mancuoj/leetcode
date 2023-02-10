878fb02a406cb36a67abf3f25e7974d17131bc00
mancuoj <1337381768@qq.com>
2023-02-11 01:20:25 +0800

    不会也看不懂

--- leetcode-daily/2-10-todo.py
+++ leetcode-daily/2-10-todo.py
@@ -1,7 +1,7 @@
 # https://leetcode.cn/problems/dice-roll-simulation/comments/
 class Solution:
     def dieSimulator(self, n: int, rollMax: List[int]) -> int:
-        mod, rollMax = int(1e9+7), [0] + rollMax
+        mod, rollMax = int(1e9 + 7), [0] + rollMax
 
         @cache
         def dfs(i, last, cnt):
@@ -10,9 +10,10 @@ class Solution:
             res = 0
             for k in range(1, 7):
                 if k == last:
-                    if cnt+1 < rollMax[k]:
-                        res = (res + dfs(i+1, k, cnt+1)) % mod
+                    if cnt + 1 < rollMax[k]:
+                        res = (res + dfs(i + 1, k, cnt + 1)) % mod
                 else:
-                    res = (res + dfs(i+1, k, 0)) % mod
+                    res = (res + dfs(i + 1, k, 0)) % mod
             return res
+
         return dfs(0, -1, 0)
