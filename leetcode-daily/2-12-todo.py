e7541ae7e63e14804f5ad890f3e354b21d3eee84
mancuoj <1337381768@qq.com>
2023-02-12 16:49:16 +0800

    不想看

---
+++ leetcode-daily/2-12-todo.py
@@ -0,0 +1,21 @@
+# https://leetcode.cn/problems/alphabet-board-path/
+class Solution:
+    def alphabetBoardPath(self, target: str) -> str:
+        cx = cy = 0
+        res = []
+        for c in target:
+            c = ord(c) - ord('a')
+            nx = c // 5
+            ny = c % 5
+            if nx < cx:
+                res.append('U' * (cx - nx))
+            if ny < cy:
+                res.append('L' * (cy - ny))
+            if nx > cx:
+                res.append('D' * (nx - cx))
+            if ny > cy:
+                res.append('R' * (ny - cy))
+            res.append('!')
+            cx = nx
+            cy = ny
+        return ''.join(res)
