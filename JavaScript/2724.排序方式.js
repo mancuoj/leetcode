dec619388403812c4eeee4a474e6b78da0a9439c
mancuoj <mancuoj@gmail.com>
2024-03-14 23:43:47 +0800

    3.14

---
+++ JavaScript/2724.排序方式.js
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.cn/problems/sort-by/
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b))
+}
